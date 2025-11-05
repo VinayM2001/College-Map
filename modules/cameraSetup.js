cadf0544828106685b3c6a934d75350328b4d6f4
VinayM2001 <70333279+VinayM2001@users.noreply.github.com>
2025-11-05 16:28:08 +0530

    Update cameraSetup.js

--- modules/cameraSetup.js
+++ modules/cameraSetup.js
@@ -1,19 +1,14 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
-const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(3, 2, 5);
+export const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 2000);
+camera.position.set(5, 4, 6);
 
-const rendererDom = document.querySelector('canvas');
-const controls = new OrbitControls(camera, rendererDom);
+export const controls = new OrbitControls(camera, document.body);
 controls.enableDamping = true;
 controls.dampingFactor = 0.05;
-
-// Adjust zoom range (distance)
-controls.minDistance = 0.5;
-controls.maxDistance = 10;
-
-// Limit vertical angle
-controls.maxPolarAngle = Math.PI * 0.95;
-
-export { camera, controls };
+controls.minDistance = 2;
+controls.maxDistance = 20;
+controls.maxPolarAngle = Math.PI * 0.48; // tilt limit
+controls.target.set(0, 0, 0);
+controls.update();
