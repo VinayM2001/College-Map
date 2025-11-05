efbccd7efe95da17dcdd8b3243e1cee05d222a5a
VinayM2001 <70333279+VinayM2001@users.noreply.github.com>
2025-11-05 16:27:51 +0530

    Update sceneSetup.js

--- modules/sceneSetup.js
+++ modules/sceneSetup.js
@@ -1,9 +1,9 @@
 import * as THREE from 'three';
 
-const scene = new THREE.Scene();
-const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+export const scene = new THREE.Scene();
+export const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(window.devicePixelRatio);
+renderer.shadowMap.enabled = true;
 document.body.appendChild(renderer.domElement);
-
-export { scene, renderer };
